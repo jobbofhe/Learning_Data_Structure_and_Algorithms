7c62425b50866d52e53dfb9af31abff477bd881f
jobbofhe <1254372617@qq.com>
2019-09-02 20:23:59 +0800

    增加伸展树部分接口代码。

--- c_cpp/008_splay_tree.c
+++ c_cpp/008_splay_tree.c
@@ -2,7 +2,7 @@
 * @Author: jobbofhe
 * @Date:   2019-08-29 10:03:17
 * @Last Modified by:   Administrator
-* @Last Modified time: 2019-08-29 10:40:52
+* @Last Modified time: 2019-09-02 20:22:53
 */
 
 /**
@@ -17,7 +17,7 @@
  * 适用场景：
  *     适合用于插入删除次数比较少，但查找多的情况。
  */
-
+#include <stdio.h>
 
 // 节点定义
 typedef int Type;
@@ -25,8 +25,8 @@ typedef int Type;
 typedef struct SplayTreeNode
 {
     Type key;
-    struct SplayTreeNode left;
-    struct SplayTreeNode right;
+    struct SplayTreeNode *left;
+    struct SplayTreeNode *right;
 
 }Node, *SplayTree;
 
@@ -65,21 +65,181 @@ Node *splaytree_search(SplayTree tree, Type key);
  */
 Node *splaytree_search_loop(SplayTree tree, Type key);
 
+/**
+ * 查找伸展树中最大的节点，并返回该节点
+ */
 Node *splaytree_search_max(SplayTree tree);
 
+/**
+ * 查找伸展树中最小的节点，并返回该节点
+ */
 Node *splaytree_search_min(SplayTree tree);
 
+/**
+ * 对伸展树执行旋转，将key对应的节点旋转为 根节点，返回根节点
+ */
 Node *splaytree_splay(SplayTree tree, Type key);
 
+/**
+ * 插入伸展树一个节点，返回根节点
+ */
 Node *splaytree_insert(SplayTree tree, Type key);
 
+/**
+ * 删除伸展树中值为key的节点，返回根节点
+ */
 Node *splaytree_delete(SplayTree tree, Type key);
 
+/**
+ * 打印伸展树的详细信息
+ */
 void splaytree_print(SplayTree tree);
 
+/**
+ * 销毁伸展树
+ */
 void splaytree_destroy(SplayTree tree);
 
+/**
+ * 创建节点
+ */
 static Node *create_node(Type key, Node *left, Node *right);
+
+// *******************************************************************************************
+
+
+void pre_order_bstree(SplayTree tree)
+{
+    if(NULL == tree)
+    {
+        return ;
+    }
+
+    printf("%d ",  tree->key);
+    pre_order_bstree(tree->left);
+    pre_order_bstree(tree->right);
+}
+
+void middle_order_bstree(SplayTree tree)
+{
+    if(NULL == tree)
+    {
+        return ;
+    }
+
+    middle_order_bstree(tree->left);
+    printf("%d ",  tree->key);
+    middle_order_bstree(tree->right);
+}
+
+void behind_order_bstree(SplayTree tree)
+{
+    if(NULL == tree)
+    {
+        return ;
+    }
+
+    behind_order_bstree(tree->left);
+    behind_order_bstree(tree->right);
+    printf("%d ", tree->key);
+}
+
+Node *splaytree_search(SplayTree tree, Type key)
+{
+    if (NULL == tree || tree->key == key)
+    {
+        return tree;
+    }
+
+    if (key < tree->key)
+    {
+        return splaytree_search(tree->left, key);
+    }
+    else
+    {
+        return splaytree_search(tree->right, key);
+    }
+}
+
+Node *splaytree_search_loop(SplayTree tree, Type key)
+{   
+    Node *p = tree;
+
+    while(p != NULL && p->key != key) 
+    {
+        if (key < p->key)
+        {
+            p = p->left;
+        }
+        else
+        {
+            p = p->right;
+        }
+    }
+
+    return p;
+}
+
+Node *splaytree_search_max(SplayTree tree)
+{
+    if (NULL == tree)
+    {
+        return NULL;
+    }
+
+    Node *p = tree;
+    while(p->right != NULL) 
+    {
+        p = p->right;
+    }
+
+    return p;
+}
+
+
+Node *splaytree_search_min(SplayTree tree)
+{
+    if (NULL == tree)
+    {
+        return NULL;
+    }
+
+    Node *p = tree;
+    while(p->left != NULL) 
+    {
+        p = p->left;
+    }
+
+    return p;
+}
+
+// 最核心的代码
+// 
+Node *splaytree_splay(SplayTree tree, Type key)
+{
+
+}
+
+Node *splaytree_insert(SplayTree tree, Type key)
+{
+
+}
+
+
+Node *splaytree_delete(SplayTree tree, Type key)
+{
+
+}
+
+void splaytree_print(SplayTree tree)
+{
+
+}
+
+void splaytree_destroy(SplayTree tree)
+{
+
+}
 
 
 int main(int argc, char const *argv[])

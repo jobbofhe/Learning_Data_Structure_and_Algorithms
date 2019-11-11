9eca2eba6686ca4a9cdb5d3cff6406b9bb33b9c8
jobbofhe <1254372617@qq.com>
2019-11-11 19:59:00 +0800

    增加注释

--- c_cpp/8_algorithm/bubble_sort.c
+++ c_cpp/8_algorithm/bubble_sort.c
@@ -2,11 +2,20 @@
 * @Author: jobbofhe
 * @Date:   2019-11-11 19:24:34
 * @Last Modified by:   Administrator
-* @Last Modified time: 2019-11-11 19:56:27
+* @Last Modified time: 2019-11-11 20:00:12
 */
 
 /**
  * 冒泡排序
+ *
+ * 冒泡排序时间复杂度
+ * 冒泡排序的时间复杂度是O(N2)。
+ * 假设被排序的数列中有N个数。遍历一趟的时间复杂度是O(N)，需要遍历多少次呢？N-1次！因此，冒泡排序的时间复杂度是O(N2)。
+ * 
+ * 冒泡排序稳定性
+ * 冒泡排序是稳定的算法，它满足稳定算法的定义。
+ * 算法稳定性 -- 假设在数列中存在a[i]=a[j]，若在排序之前，a[i]在a[j]前面；并且排序之后，a[i]仍然在a[j]前面。则这个排序算法是稳定的！
+ * 
  */
 #include <stdio.h>
 
@@ -93,3 +102,4 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
+

2ffc74e4f959371947dd70e8a7a007cb5a05cb79
jobbofhe <1254372617@qq.com>
2020-08-11 22:56:40 +0800

    [add]增加注释

--- shuati/code/yuesefuhuan.c
+++ shuati/code/yuesefuhuan.c
@@ -2,14 +2,23 @@
 * @Author: shuqiang
 * @Date:   2020-08-10 20:39:13
 * @Last Modified by:   Administrator
-* @Last Modified time: 2020-08-11 21:43:44
+* @Last Modified time: 2020-08-11 22:56:23
 */
 
+/**
+ * 问题：
+ * N个人围成一圈，第一个人从1开始报数，报M的将被杀掉，下一个人接着从1开始报。如此反复，最后剩下一个，求最后的胜利者。
+ *
+ * 思路：
+ * 1. 循环列表完成，每次被“杀掉的人”，其节点被删除，时间复杂度 O(mn)
+ * 2. 公式法
+ * 因此我们可以推出递推公式f(8,3) = [f(7, 3) + 3] \% 8f(8,3)=[f(7,3)+3]%8
+ * 进行推广泛化，即f(n,m) = [f(n-1, m) + m] \% nf(n,m)=[f(n−1,m)+m]%n
+ */
 
 
 int joseph(int m, int k)
 {
-
 	int position = 0;
 
 	for (int i = 2; i <= m; ++i)

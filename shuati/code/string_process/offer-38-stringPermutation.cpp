ff24c55ef3c6966dfe8c9a7131df75aa1b347cf8
jobbofhe <1254372617@qq.com>
2020-08-14 19:23:44 +0800

    [add] 增加字符串全排列问题

---
+++ shuati/code/string_process/offer-38-stringPermutation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+void backtrack(std::string s, int start, std::set<std::string> &res)
+{
+	// 回溯结束条件
+	if (start == s.size())
+	{
+		res.insert(s);
+		return;
+	}
+
+	for (int i = start; i < s.size(); ++i)
+	{
+		std::swap(s[i], s[start+1]);
+		backtrack(s, start+1, res);
+		std::swap(s[i], s[start+1]);
+	}
+}
+
+std::vector<std::string> permutation(std::string s)
+{
+	// 去重处理
+	std::set<std::string> res;
+	backtrack(s, 0, res);
+
+	return std::vector<std::string>(res.begin(), res.end());
+}
+
+int main(int argc, char const *argv[])
+{
+	std::string src("jobfhe");
+	std::cout << src << std::endl;
+	std::vector<std::string> res = permutation(src);
+	std::cout << res.size() << std::endl;
+
+	return 0;
+}

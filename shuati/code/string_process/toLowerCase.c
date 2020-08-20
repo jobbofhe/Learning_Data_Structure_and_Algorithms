6f65bfb2302f9f1b4ecf5a2c0c1c79e9eb57cc0e
jobbofhe <1254372617@qq.com>
2020-08-20 21:07:05 +0800

    [update] 位运算处理大小写转换问题

--- shuati/code/string_process/toLowerCase.c
+++ shuati/code/string_process/toLowerCase.c
@@ -2,7 +2,7 @@
 * @Author: shuqiang
 * @Date:   2020-08-20 20:17:21
 * @Last Modified by:   Administrator
-* @Last Modified time: 2020-08-20 20:43:03
+* @Last Modified time: 2020-08-20 21:06:56
 */
 
 #include <stdio.h>
@@ -62,8 +62,51 @@ void test()
 }
 
 
+/**
+ * 位运算
+ * 大写变小写、小写变大写 : 字符 ^= 32;
+ * 大写变小写、小写变小写 : 字符 |= 32;
+ * 小写变大写、大写变大写 : 字符 &= -33;
+ */
+
+char *toLowerCase_v2(char *str)
+{
+	char *tmp = (char*)malloc(sizeof(char)*(strlen(str)+1));
+
+	int i = 0;
+	while(*str != '\0') {
+
+		// tmp[i] = *str^32;
+
+		tmp[i] = *str|32;
+
+		i++;	
+		str++;
+	}
+	tmp[i] = '\0';
+
+	return tmp;
+}
+
+void test2()
+{
+	char *str = (char*)malloc(sizeof(char)*36);
+	strcpy(str, "HASLKalkjdsALKDSH");
+
+	char *res = toLowerCase_v2(str);
+
+	printf("src:%s\n", str);
+	printf("dst:%s\n", res);
+
+	free(str);
+	free(res);
+}
+
 int main(int argc, char const *argv[])
 {
 	test();
+
+	test2();
+
 	return 0;
 }

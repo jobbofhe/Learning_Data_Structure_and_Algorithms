e005ea14c48f0621d8c7f85221568a08291cecff
jobbofhe <1254372617@qq.com>
2020-08-20 21:47:04 +0800

    [add] 计算最后一个单词的长度：反向遍历

---
+++ shuati/code/string_process/lengthOfLastWord.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int lengthOfLastWord(char * s)
+{
+	int len = strlen(s);
+	char *p = s;
+	int cnt = 0;
+
+	if (len == 0)
+	{
+		return 0;
+	}
+
+	for (int i = len-1; i >= 0; --i)
+	{
+		if (p[i] != ' ')
+		{
+			cnt++;
+		}
+		else if (cnt != 0)
+		{
+			break;
+		}
+	}
+
+	return cnt;
+}
+
+void test()
+{
+	char a[36] = "hello leetoce java dachang";
+
+	int count = lengthOfLastWord(a);
+
+	printf("src: %s\n", a);
+	printf("count: %d\n", count);
+}
+
+int main(int argc, char const *argv[])
+{
+	test();
+
+	return 0;
+}
